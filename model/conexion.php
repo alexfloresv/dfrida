5492d58103f5767106b7f63cdca55de5397a5971
Alx <worfysito15@gmail.com>
2024-07-02 16:27:02 -0500

    linea de conecion ala base de datos hostinguer

--- model/conexion.php
+++ model/conexion.php
@@ -8,3 +8,13 @@ class Conexion
     return $link;
   }
 }
+//conectar a la base de datos hostinger
+/* class Conexion
+{
+static public function conn()
+{
+$link = new PDO("mysql:host=localhost;dbname=u553466910_dfrida","u553466910_admin_dfrida","/j1/jkU5");
+$link->exec("set names utf8");
+return $link;
+}
+} */
